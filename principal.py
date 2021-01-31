7e0d55b55878d5bc5552952f59df634ec52ebb53
MiaouLeLion <nicolas-berthier@orange.fr>
2021-01-31 20:28:43 +0100

    ajout creation de graph

--- principal.py
+++ principal.py
@@ -2,7 +2,7 @@ from operator import itemgetter, attrgetter
 import fileinput
 
 Arretes=[]
-Vertex={}
+
 Arborescence=[]
 
 
@@ -11,17 +11,32 @@ for line in fileinput.input():
         line=line.split()
         Arretes.append((int(line[0]),int(line[1]))) 
 
-        
+Arretes=sorted(Arretes)
 connexe={}
 
-for arrete in Arretes:
-    arretePrec=()
-    if arretePrec!=arrete:
-        connexe
-        arretePrec=arrete
 
-#print(sorted(Arretes))
-print(Arretes)
-print(Vertex)
+
+def CreerGraphe(Graph,Arretes):
+    i=1
+    Graph={}
+#for i in range(1,len(Arretes)-1,1):
+    while(i<len(Arretes)-1):
+        voisins=[]
+        voisins.append(Arretes[i-1][1])
+        while(Arretes[i-1][0]==Arretes[i][0]):
+            voisins.append(Arretes[i][1])
+            i=i+1
+        Graph[Arretes[i-1][0]]=voisins
+        i=i+1
+    return Graph
+
+g={}
+G=CreerGraphe(g,Arretes)
+
+
+print(Arretes[20][0])
+print(sorted(Arretes))
+print(len(Arretes))
+print(G)
 
 fileinput.close()
---
+++ test.py
@@ -0,0 +1,8 @@
+i=0
+
+for i in range(0,10):
+    y=0
+    while(y<3):
+        y=y+1
+        i=i+1
+    print(i)
