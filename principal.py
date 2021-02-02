8ad0be14100cd10be79ab2e74399a6f2ad663134
MiaouLeLion <nicolas-berthier@orange.fr>
2021-02-02 22:20:41 +0100

    algo suppression arretes

--- principal.py
+++ principal.py
@@ -33,17 +33,26 @@ for v in Graph:
 #Graph=AjouterArreteGraph(Graph,2,7)
 #Graph=SupprimerArreteGraph(Graph,2,8)
 UnionClique=False
-
+k=1
 #Tant que ce n'est pas une union de clique, on continue
 while not UnionClique:
     ListClique=list(nx.enumerate_all_cliques(Graph))
-    PlusGrandeClique=ListClique[len(ListClique)-1]
+    print(k)
+    print(ListClique)
+    PlusGrandeClique=ListClique[len(ListClique)-1*k]
     for v in PlusGrandeClique:
         if len(PlusGrandeClique)-1<Graph.degree[v]:
-            VoisinsExterieursCliques=set(neighbors(Graph,v))
-            SommetASupprimer=VoisinsExterieursCliques-set(PlusGrandeClique)
-            Graph.remove_edge(v,2)
-    UnionClique=True
+            VoisinsExterieursCliques=set(nx.neighbors(Graph,v))
+            SommetASupprimer=list(VoisinsExterieursCliques-set(PlusGrandeClique))
+            for w in SommetASupprimer:
+                Graph.remove_edge(v,w)
+                print(str(v) + " " + str(w))
+                #IL FAUT MARQUER LES SOMMETS DE CETTE CLIQUE POUR 
+                #PROTEGER LEURS ARRETES INTERNES A LA CLIQUE
+    ListClique.remove(PlusGrandeClique)
+    k=k+1
+    if k>4:
+        UnionClique=True
     #for v in Graph:
         #if Graph.degree[v]>
 
