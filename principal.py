bd1e2a8ecd0d71a2e689362113679dc7c7fc2b9b
MiaouLeLion <nicolas-berthier@orange.fr>
2021-02-06 18:07:59 +0100

    amelioration creation graph sans fonction

--- principal.py
+++ principal.py
@@ -10,14 +10,10 @@ for line in fileinput.input():
     if line[0].isdigit():
         line=line.split()
         Graph[int(line[0])].append(int(line[1]))
-        Graph[int(line[1])].append(int(line[0]))
-        #Arretes.append((int(line[0]),int(line[1]))) 
+        Graph[int(line[1])].append(int(line[0])) 
         
-#print(Graph)
+
 fileinput.close()
-
-#Arretes=sorted(Arretes)
-
 
 
 def CreerGraphe(Arretes):
@@ -26,24 +22,6 @@ def CreerGraphe(Arretes):
         Graph[node1].append(node2)
         Graph[node2].append(node1)
     return Graph
-
-#Creation du graph (dictionnaire) a partir des input.
-#Graph=CreerGraphe(Arretes)
-
-
-
-def supprimerArete(Graphe, u,v):
-    i = 0
-    while i < len(Graphe[u]):
-        if Graphe[u][i] == v:
-            del Graphe[u][i]
-        i+=1
-    j = 0
-    while j < len(Graphe[v]):
-        if Graphe[v][j] == u:
-            del Graphe[v][j]
-        j+=1
-    return Graphe
 
 
 
@@ -87,20 +65,6 @@ def ListerArreteAjoutee(List,node1,node2):
 
 
 
-
-
-
-#Renvoie les voisins d'un sommet et leur % de voisins partages
-def GetDegreDeConnexionNode(Graph,node):
-    voisins=set(Graph[node])
-    nbrVoisin=float(len(voisins))
-    ListeDegre=[]
-    for v in voisins:
-        voisins_2nd=set(Graph[int(v)])
-        degSommet=float(len(voisins & voisins_2nd)+1)
-        ListeDegre.append((degSommet/nbrVoisin))
-    return ListeDegre
-
 def GetDegreNode(Graph,node):
     return len(Graph[node])
 
@@ -109,7 +73,7 @@ def GetVoisins(Graph,node):
     return Graph[node]
 
 
-
+#Renvoie un sous-graph complet à partir d'un sommet pris dans le Graph
 def GetClique(Graph,node):
     ListeClique=set()
     ListeClique.add(node)
@@ -144,17 +108,13 @@ def ClasserParDegree(Graph):
         Classement.append((v,GetDegreNode(Graph,v)))
     return sorted(Classement,key=itemgetter(1), reverse=True)
 
-
+#Permet de classer les sommets par degré, prochaine amélioration
 GraphByDegree=ClasserParDegree(Graph)
 
 
-
-#for i in A:
-    #print(i)
-
-
-
-for s in Graph:#Faire une copie du graph et supprimer les arrêtes
+#Algo principale de suppression des arrêtes
+#autour des cliques potentielles
+for s in Graph:
     Clique=GetClique(Graph,s)
     for v in Clique:
         if len(Clique)-1<GetDegreNode(Graph,v):
@@ -163,17 +123,13 @@ for s in Graph:#Faire une copie du graph et supprimer les arrêtes
             ListeASupprimer=list(ListeASupprimer)
             for w in ListeASupprimer:
                 Graph=SupprimerArreteGraph(Graph,v,w)
-
+                print(str(v) + " " + str(w))
         
   
 
-#B=ClasserParDegree(Graph)
-#print("************")
-#for i in B:
-    #print(i)
-
-#for v in Graph:
-    #print(EstUneClique(Graph,v))
+#Vérification si les sommets forment bien des cliques
+for v in Graph:
+    print(EstUneClique(Graph,v))
 
 
 
